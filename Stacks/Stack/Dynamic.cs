7096ae8996ac445420347f297f93d70b73ca745a
Ezekiela <ezekielaanderson@.com>
2021-06-17 08:22:50 -0700

    stacks commit

---
+++ Stacks/Stack/Dynamic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Stack
+{
+    class Dynamic
+    {
+        public int top { get; set; }
+        public int max { get; set; }
+        public int[] a { get; set; }
+        public Dynamic() {
+            this.top = -1;
+            this.max = 5;
+            this.a = new int[this.max];
+        }
+        //use the data structure linked list
+    }
+}
---
+++ Stacks/Stack/Static.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Stack
+{
+    class Static
+    {
+        public int top { get; set; }
+        public int max { get; set; }
+        public int[] a { get; set; }
+        public Static() {
+            this.top = -1;
+            this.max = 5;
+            this.a=new int[this.max];
+        }
+
+        public void push(int item) {
+            if (this.top == this.max - 1)
+                Console.WriteLine("Stack Overflow");
+            else {
+                this.top ++;
+                a[top] = item;
+            }
+        }
+
+        public void pop() {
+            if (this.top != -1)
+                top--;
+            else
+                Console.WriteLine("Underflow");
+        }
+    }
+}
